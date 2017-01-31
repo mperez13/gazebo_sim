3089fcfc93795d7d7d81ec87ffffb425e065c4ec
mfperez13 <mfperez13@gmail.com>
2017-01-30 18:08:13 -0800

    first

---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/marifer/mybot_ws/devel:/opt/ros/kinetic:/home/marifer/simbot_ws/devel"
+export LD_LIBRARY_PATH="/opt/ros/kinetic/lib:/home/marifer/simbot_ws/devel/lib"
+export PKG_CONFIG_PATH="/opt/ros/kinetic/lib/pkgconfig:/home/marifer/simbot_ws/devel/lib/pkgconfig"
+export PWD="/home/marifer/mybot_ws/build"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/marifer/mybot_ws/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/marifer/mybot_ws/src:$ROS_PACKAGE_PATH"
